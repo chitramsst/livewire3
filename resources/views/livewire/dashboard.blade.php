91282dd877ae27646f1e50c457a98e00b456495f
Chitra <chitra@192.168.1.7>
2023-08-02 09:53:03 +0530

    model

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,49 +1,91 @@
-<div>
-       <!-- Stats Row Starts Here -->
-       <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2">
-                        <div class="shadow-lg bg-red-vibrant border-l-8 hover:bg-red-vibrant-dark border-red-vibrant-dark mb-2 p-2 md:w-1/4 mx-2">
-                            <div class="p-4 flex flex-col">
-                                <a href="#" class="no-underline text-white text-2xl">
-                                    $244
-                                </a>
-                                <a href="#" class="no-underline text-white text-lg">
-                                    Total Sales
-                                </a>
-                            </div>
-                        </div>
+<div x-data="{open:false}">
+    <!-- Stats Row Starts Here -->
+    <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2">
+        <div class="shadow-lg bg-red-vibrant border-l-8 hover:bg-red-vibrant-dark border-red-vibrant-dark mb-2 p-2 md:w-1/4 mx-2">
+            <div class="p-4 flex flex-col">
+                <a href="#" class="no-underline text-white text-2xl">
+                    $244
+                </a>
+                <a href="#" class="no-underline text-white text-lg">
+                    Total Sales
+                </a>
+            </div>
+        </div>
 
-                        <div class="shadow bg-info border-l-8 hover:bg-info-dark border-info-dark mb-2 p-2 md:w-1/4 mx-2">
-                            <div class="p-4 flex flex-col">
-                                <a href="#" class="no-underline text-white text-2xl">
-                                    $199.4
-                                </a>
-                                <a href="#" class="no-underline text-white text-lg">
-                                    Total Cost
-                                </a>
-                            </div>
-                        </div>
+        <div class="shadow bg-info border-l-8 hover:bg-info-dark border-info-dark mb-2 p-2 md:w-1/4 mx-2">
+            <div class="p-4 flex flex-col">
+                <a href="#" class="no-underline text-white text-2xl">
+                    $199.4
+                </a>
+                <a href="#" class="no-underline text-white text-lg">
+                    Total Cost
+                </a>
+            </div>
+        </div>
 
-                        <div class="shadow bg-warning border-l-8 hover:bg-warning-dark border-warning-dark mb-2 p-2 md:w-1/4 mx-2">
-                            <div class="p-4 flex flex-col">
-                                <a href="#" class="no-underline text-white text-2xl">
-                                    900
-                                </a>
-                                <a href="#" class="no-underline text-white text-lg">
-                                    Total Users
-                                </a>
-                            </div>
-                        </div>
+        <div class="shadow bg-warning border-l-8 hover:bg-warning-dark border-warning-dark mb-2 p-2 md:w-1/4 mx-2">
+            <div class="p-4 flex flex-col">
+                <a href="#" class="no-underline text-white text-2xl">
+                    900
+                </a>
+                <a href="#" class="no-underline text-white text-lg">
+                    Total Users
+                </a>
+            </div>
+        </div>
 
-                        <div class="shadow bg-success border-l-8 hover:bg-success-dark border-success-dark mb-2 p-2 md:w-1/4 mx-2">
-                            <div class="p-4 flex flex-col">
-                                <a href="#" class="no-underline text-white text-2xl">
-                                    500
-                                </a>
-                                <a href="#" class="no-underline text-white text-lg">
-                                    Total Products
-                                </a>
-                            </div>
+        <div class="shadow bg-success border-l-8 hover:bg-success-dark border-success-dark mb-2 p-2 md:w-1/4 mx-2">
+            <div class="p-4 flex flex-col">
+                <a href="#" class="no-underline text-white text-2xl">
+                    500
+                </a>
+                <a href="#" class="no-underline text-white text-lg">
+                    Total Products
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class=" justify-center shadow items-center flex flex-col m-5" @click="open=!open">
+        <div class=" rounded-md bg-gray-500  shadow-md py-2 px-5 text-white"> click </div>
+    </div>
+
+    <div x-show="open" class="fixed inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center">
+        <div x-show="open" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 transition-opacity">
+            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+        <div x-show="open" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div class="sm:flex sm:items-start">
+                    <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <svg class="h-6 w-6 text-red-600" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                        </svg>
+                    </div>
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <h3 class="text-lg leading-6 font-medium text-gray-900">
+                            Deactivate account
+                        </h3>
+                        <div class="mt-2">
+                            <p class="text-sm leading-5 text-gray-500">
+                                Are you sure you want to deactivate your account? All of your data will be permanantly removed. This action cannot be undone.
+                            </p>
                         </div>
                     </div>
-
+                </div>
+            </div>
+            <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+                    <button @click="open = false; setTimeout(() => open = true, 1000)" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                        Deactivate
+                    </button>
+                </span>
+                <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+                    <button @click="open = false; " type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                        Cancel
+                    </button>
+                </span>
+            </div>
+        </div>
+    </div>
 </div>
